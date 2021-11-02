9cf1a052712b2bcd8ce4a78a77441f1c81e59866
CodeDude404 <68038925+CodeDude404@users.noreply.github.com>
2021-11-02 16:16:02 -0700

    Do some debug stuff

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from log import Log
 logger = Log()
 
 logger.db("Began PyGame initialization...")
+
 pygame.init() 	# intitalize pygame
+pygame.display.set_caption('Tutorial 1')
+
 logger.db("Began initialized PyGame.")
 
 logger.db("Making varibles.")
@@ -29,19 +32,21 @@ logger.db("Made varibles.")
 
 def main():
 	global screen
+	pygame.fullscreen = False
 	while True:
 		clock.tick(60)
 		# logger.db("Clock tick sucessful.")
 
 		for event in pygame.event.get():
-			if event.type == QUIT:
+			if event.type == pygame.QUIT:
 				logger.crit("User forced exit. Goodbye!")
+				pygame.quit()
 				sys.exit()
 			
-			elif event.type == KEYDOWN:
-				if event.key == K_f:
-					screen = pygame.display.set_mode(size,FULLSCREEN)
-				elif event.key == K_ESCAPE:
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_f:
+					screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+				elif event.key == pygame.K_ESCAPE:
 					screen = pygame.display.set_mode(size)
 
 		screen.fill(color)
